c7d2baddee2e0546c87a16d3c4d4130f4a41b1f9
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2012-07-23 01:15:42 +0100

    Rearrange py and cy module names

---
+++ py_something.py
@@ -0,0 +1,6 @@
+
+def identity(N):
+    total = 0
+    for n in range(N):
+        total += 1
+    return total
--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-setup(ext_modules = [Extension('something2', ['something.py'])],
+setup(ext_modules = [Extension('cy_something', ['py_something.py'])],
       cmdclass = {'build_ext':build_ext})
